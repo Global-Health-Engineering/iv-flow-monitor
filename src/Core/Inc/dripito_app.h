fe2f7c7c9722066aee4ac481a21c9499ba6e99b6
TheDeathrae <fionn.smith@protonmail.com>
2026-02-27 21:55:54 +0100

    File upload

---
+++ src/Core/Inc/dripito_app.h
@@ -0,0 +1,10 @@
+#ifndef DRIPITO_APP_H
+#define DRIPITO_APP_H
+
+#include <stdint.h>
+
+/* Shared global variable (declaration only) */
+extern uint16_t DRIP_FACTOR_GTT_PER_ML;
+extern uint16_t adjust_drop_volume;
+
+#endif
